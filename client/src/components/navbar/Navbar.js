5663edafc7ea5dc2d2d70440368f5df7c1de4247
Sakshi Dwivedi <sakshithedwivedi@gmail.com>
2021-05-21 20:24:06 +0530

    Adding navbar and footer

---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <p className="logo"></p>
+            <p className="name">Name</p>
+            <p className="reflection">Name</p>
+        </div>
+    )
+}
+
+export default Navbar;
