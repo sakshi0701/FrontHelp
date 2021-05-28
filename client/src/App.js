f640016361e9f875f83edd4bb6078ac415cc664c
Sakshi Dwivedi <sakshithedwivedi@gmail.com>
2021-05-28 12:41:16 +0530

    Adding code functionality

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,13 @@ import BtnMain from './components/Pages/ButtonsPg/BtnMain';
 import CardsMain from './components/Pages/CardsPg/CardsMain';
 import SpinMain from './components/Pages/Spinners/SpinMain';
 import BadgeMain from './components/Pages/Badges/BadgeMain';
-
-import Signup from "./components/Save/authentication/Signup"
-import { AuthProvider } from "./components/Save/contexts/AuthContext"
-import Login from "./components/Save/authentication/Login"
-import PrivateRoute from "./components/Save/authentication/PrivateRoute"
-import ForgotPassword from "./components/Save/authentication/ForgotPassword"
-import SaveMain from "./components/Save/google-drive/Dashboard"
+import Signup from "./components/Save/authentication/Signup";
+import { AuthProvider } from "./components/Save/contexts/AuthContext";
+import Login from "./components/Save/authentication/Login";
+import PrivateRoute from "./components/Save/authentication/PrivateRoute";
+import ForgotPassword from "./components/Save/authentication/ForgotPassword";
+import SaveMain from "./components/Save/google-drive/Dashboard";
+import Code from './components/Code/Code';
 import Footer from './components/footer/Footer';
 
 function App() {
@@ -34,12 +34,13 @@ function App() {
             <Route path="/components/cards" exact component={CardsMain} />
             <Route path="/components/spinners" exact component={SpinMain} />
             <Route path="/components/badges" exact component={BadgeMain} />
-
             <PrivateRoute exact path="/save" component={SaveMain} />
             <PrivateRoute exact path="/folder/:folderId" component={SaveMain} />
             <Route path="/signup" component={Signup} />
             <Route path="/login" component={Login} />
             <Route path="/forgot-password" component={ForgotPassword} />
+            <Route path="/code" exact component={Code} />
+
           </Switch>
         </div>
         <Footer />
